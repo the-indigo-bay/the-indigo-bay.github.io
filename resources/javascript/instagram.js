8d6563bf0b9c0365f3147553e106f5f1e1cbc446
Amias Burrows <amias.burrows@gmail.com>
2021-02-05 11:09:34 +0000

    EDIT RESOURCES
    I have fixed some css files to make the code be more inclusive to all
    elements such as lists and links to music sites.

--- resources/javascript/instagram.js
+++
@@ -1,7 +0,0 @@
-var userFeed = new Instafeed({
-	get: 'indigo',
-	target: "instafeed-container",
-	resolution: 'low_resolution',
-	accessToken: 'YOUR_INSTAGRAM_ACCESS_TOKEN_GOES_HERE'
-});
-userFeed.run();
