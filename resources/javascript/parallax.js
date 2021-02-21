83388600f22b16f7ca1c933382971ff17d238a56
Amias-Burrows <amias.burrows@gmail.com>
2021-02-21 11:15:38 +0000

    MORE IMAGES
    I have added more images to the website parallax section

--- resources/javascript/parallax.js
+++ resources/javascript/parallax.js
@@ -1,5 +1,5 @@
 var parallax = document.querySelectorAll(".shiftImage");
-var images = ['img1', 'img2', 'img3'];
+var images = ['img1', 'img2', 'img3', 'img4', 'img5', 'img6', 'img7', 'img8', 'img9', 'img10', 'img11', 'img12', 'img13', 'img14', 'img15', 'img16', 'img17', 'img18', 'img19', 'img20', 'img21'];
 
 for (let i = images.length - 1; i >= 0; i--)
 {
