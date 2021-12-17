97a27cfb4c75ef472b9d94397a04bd9b8173795e
amias-burrows <amias.burrows@gmail.com>
2021-12-17 15:01:19 +0000

    Fix bio

---
+++ res/js/bio.js
@@ -0,0 +1,59 @@
+var name = document.querySelector('#name').innerHTML;
+var output = document.querySelector('#age');
+
+var ages = {
+	alex: {
+		d: '9',
+		m: '6',
+		y: '2000'
+	},
+	amias: {
+		d: '5',
+		m: '6',
+		y: '1999'
+	},
+	callum: {
+		d: '11',
+		m: '11',
+		y: '2000'
+	},
+	george: {
+		d: '29',
+		m: '6',
+		y: '2001'
+	}
+};
+
+var d = new Date().getDate();
+var m = new Date().getMonth() + 1;
+var y = new Date().getFullYear();
+
+switch (name) {
+	case 'Alex Plater':
+		output.innerHTML += ' - (' + getAge(ages.alex) + ')';
+		break;
+	case 'Amias Burrows':
+		output.innerHTML += ' - (' + getAge(ages.amias) + ')';
+		break;
+	case 'Callum Minks':
+		output.innerHTML += ' - (' + getAge(ages.callum) + ')';
+		break;
+	case 'George Gray':
+		output.innerHTML += ' - (' + getAge(ages.george) + ')';
+		break;
+}
+
+
+function getAge(age) {
+	if (age.m > m) {
+		return y - age.y;
+	} else if (age.m == m) {
+		if (age.d >= d) {
+			return (y + 1) - age.y;
+		} else {
+			return y - age.y;
+		}
+	} else {
+		return y - age.y;
+	}
+}
