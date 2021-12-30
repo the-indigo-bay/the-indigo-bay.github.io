089544ce3a99338f5653c31748c0d5a492e28a8d
amias-burrows <amias.burrows@gmail.com>
2021-12-30 10:37:51 +0000

    Cover up issue on IOS

--- res/js/parallax.js
+++ res/js/parallax.js
@@ -1,15 +1,22 @@
 var parallax = document.querySelectorAll(".parallax");
-var images = ['img1', 'img2', 'img3', 'img4', 'img5', 'img6', 'img7', 'img8', 'img9', 'img10', 'img11', 'img12', 'img13', 'img14', 'img15', 'img16', 'img17', 'img18', 'img19', 'img20', 'img21'];
 
-for (let i = images.length - 1; i >= 0; i--)
-{
-	const j = Math.floor(Math.random() * i);
-	const temp = images[i];
-	images[i] = images[j];
-	images[j] = temp;
-};
+var isIOS = (function () {
+	var iosQuirkPresent = function () {
+		var audio = new Audio();
 
-for (var i = 0; i < parallax.length; i++)
-{
-	parallax[i].classList.add(images[i]);
+		audio.volume = 0.5;
+		return audio.volume === 1;
+	};
+
+	var isios = /iPad|iPhone|iPod/.test(navigator.userAgent);
+	var isAppleDevice = navigator.userAgent.includes('Macintosh');
+	var isTouchScreen = navigator.maxTouchPoints >= 1;
+
+	return isios || (isAppleDevice && (isTouchScreen || iosQuirkPresent()));
+});
+
+if (isIOS) {
+	parallax.forEach(function(value) {
+		value.classList.add('ios');
+	});
 };
